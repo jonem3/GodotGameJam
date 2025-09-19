bf407aa7fcdd4803c711e09638845725afbb929e
jjhamer123 <jjhamer123@gmail.com>
2025-09-19 13:28:12 +0100

    added the parts for the wings to grow out

--- testforgamejam/Boss/boss_wings.gd
+++ testforgamejam/Boss/boss_wings.gd
@@ -1,20 +1,23 @@
 extends Node2D
-var wing_route = load("res://Boss/wing/wing_route.tscn")
+var wing_route = preload("res://Boss/wing/wing_route.tscn")
 
 
-func ready() -> void:
-	var wing_points=get_children()
+func _ready() -> void:
 
-	for wing in wing_points:
-		
+	for wing in get_children():
+		var wingscene = wing_route.instantiate()
 		#set the wing to see if it is right or left of the main eye and transform as needed
-		rightorleftofeye()
-		
+		rightorleftofeye(wing)
+		print(wing)
 		#adds in a wing at each of the point
-		
-		
+		wing.add_child(wingscene)
 		pass
 	pass
 	
-func rightorleftofeye():
+	
+func rightorleftofeye(point):
+	#inverts the thing depending on what side the wing is on
+	if point.position.x >= 0:
+		point.scale.x*=-1
+		pass
 	pass
